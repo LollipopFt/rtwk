b2bd941fb6484b5e19e7ec2661ebd756cb8433ef
LollipopFt <lollipopz1426@gmail.com>
2023-09-21 22:45:12 +0800

    4.2: Sending Rays Into the Scene

--- src/ray.rs
+++ src/ray.rs
@@ -7,7 +7,17 @@ pub struct Ray {
 }
 
 impl Ray {
-  fn at(&self, t: Flt) -> Vec3 {
+  pub fn new(orig: Vec3, dir: Vec3) -> Self {
+    Self { orig, dir }
+  }
+
+  pub fn at(&self, t: Flt) -> Vec3 {
     self.orig + t * self.dir
+  }
+
+  pub fn colour(&self) -> Vec3 {
+    let unit_dir = self.dir.normalize();
+    let a = 0.5 * (unit_dir.y + 1.);
+    (1. - a) * Vec3::ONE + a * Vec3::new(0.5, 0.7, 1.)
   }
 }
